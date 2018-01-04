17285b87f589a1db1114201af9d8429dec54f0f5
Ricardo Devis Agullo <mkredz@gmail.com>
2018-01-04 12:01:47 +0100

    move build to cjs and also add es module version

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
 
 export default {
   entry: './src/main.ts',
-  output: {
-    file: './dist/bundle.js',
-    format: 'iife',
-    name: 'Hitter',
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
   plugins: [
     typescript({
       typescript: require('typescript'),
